c33a5aa3216cf8777a7f8ca6e8d29cd1d63efd90
hchen <hchen229@gmail.com>
2017-03-27 16:59:57 -0700

    1. Add ssl certificate for api.sleepaiden.com. 2. Modify install ssl script to take domain as the second parameter.

--- install_ssl.sh
+++ install_ssl.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 # $1: is the zip file that contains ssl certificate
+# $2: the domain, e.g. sleepaiden.com or api.sleepaiden.com
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: ./install_ssl.sh  <ssl zip file> <domain>"
+  exit 1
+fi
 
 # delete temporary files if exist
-rm -rf ca_bundle.crt certificate.crt private.key sleepaiden.com.crt
+rm -rf ca_bundle.crt certificate.crt private.key $2.crt
 
 # unzip ssl files
 unzip $1
 
 # generate chained certificate
-(cat certificate.crt; echo ""; cat ca_bundle.crt;) > sleepaiden.com.crt
+(cat certificate.crt; echo ""; cat ca_bundle.crt;) > $2.crt
 
 # create certificate dir if not exists
 sudo mkdir -p /etc/nginx/ssl
 
 # move files
-sudo mv private.key /etc/nginx/ssl/sleepaiden.com.key
-sudo mv sleepaiden.com.crt /etc/nginx/ssl/sleepaiden.com.crt
+sudo mv private.key /etc/nginx/ssl/$2.key
+sudo mv $2.crt /etc/nginx/ssl/$2.crt
 
 # clean
 
