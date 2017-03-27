7f4005aabc73e226e6dc80f5811a27ef90ed04d4
hchen <hchen229@gmail.com>
2017-03-27 12:17:17 -0700

    add install ssl shell and the certificate file of 2017-03-27

---
+++ install_ssl.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# $1: is the zip file that contains ssl certificate
+
+# delete temporary files if exist
+rm -rf ca_bundle.crt certificate.crt private.key sleepaiden.com.crt
+
+# unzip ssl files
+unzip $1
+
+# generate chained certificate
+cat certificate.crt ca_bundle.crt > sleepaiden.com.crt
+
+# create certificate dir if not exists
+sudo mkdir -p /etc/nginx/ssl
+
+# move files
+sudo mv private.key /etc/nginx/ssl/sleepaiden.com.key
+sudo mv sleepaiden.com.crt /etc/nginx/ssl/sleepaiden.com.crt
+
+# clean
+
+rm -rf ca_bundle.crt certificate.crt
